9e2ead0fcfe402cb5b04c2dd5caf0f3f0c9daf8a
Kim-Linh Vu <kimlinh@fb.com>
2022-06-24 10:40:59 -0700

    centered product-view product-card

--- student-store-ui/src/components/ProductView/ProductView.jsx
+++ student-store-ui/src/components/ProductView/ProductView.jsx
@@ -14,16 +14,18 @@ export const ProductView = ({
   return (
     <div className='product-view'>
       <Hero />
-      <h1 className='product-id'>Product #{productId}</h1>
-      <ProductCard
-        product={product}
-        productId={productId}
-        shoppingCart={shoppingCart}
-        quantity={quantity}
-        handleAddItemToCart={handleAddItemToCart}
-        handleRemoveItemToCart={handleRemoveItemToCart}
-        showDescription={true}
-      />
+      <div className="contents">
+        <h1 className='product-id'>Product #{productId}</h1>
+        <ProductCard
+          product={product}
+          productId={productId}
+          shoppingCart={shoppingCart}
+          quantity={quantity}
+          handleAddItemToCart={handleAddItemToCart}
+          handleRemoveItemToCart={handleRemoveItemToCart}
+          showDescription={true}
+        />
+      </div>
     </div>
   )
 }
